4cb3c458c92cb7cfa7909f177c820be46b14ebf8
unknown <xnerhu@gmail.com>
2019-01-19 20:13:45 +0100

    Improve user profile

--- src/client/components/Home/index.tsx
+++ src/client/components/Home/index.tsx
@@ -9,7 +9,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <AppBar className="appbar" position="relative">
+        <AppBar className="appbar" position="relative" color="inherit">
           <Toolbar>
             <Typography
               className="appbar-typography"
--- src/client/components/Profile/index.tsx
+++ src/client/components/Profile/index.tsx
@@ -1,46 +1,27 @@
 import * as React from 'react';
+import { Typography } from '@material-ui/core';
 
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import './styles.scss';
 
 export default class Profile extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <AppBar className="appbar profile-appbar" position="relative">
-          <Toolbar>
-            <IconButton
-              style={{ marginLeft: -12, marginRight: 20 }}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <div className="appbar-arrow-icon" />
-            </IconButton>
-            <Typography
-              className="appbar-typography"
-              variant="h6"
-              color="inherit"
-            >
-              Profile
-            </Typography>
-          </Toolbar>
-          <div className="profile-presentation">
-            <div className="profile-presentation-avatar" />
-            <Typography
-              className="profile-presentation-fullname"
-              variant="h4"
-              color="inherit"
-            >
-              Jan Kowalski
-            </Typography>
-          </div>
-        </AppBar>
+        <div className="profile-background-image" />
+        <div className="profile-avatar" />
+        <Typography className="profile-username" variant="h5" color="inherit">
+          Mikołaj Palkiewicz
+        </Typography>
         <div className="profile-description">
-          <Typography variant="body1" color="inherit">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo saepe
-            eligendi modi quidem eius fugit provident facilis dolores qui et
-            natus, ipsa nostrum, quis iure pariatur assumenda cupiditate eaque
-            libero?
+          <Typography
+            className="appbar-typography"
+            variant="body1"
+            color="inherit"
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
+            nesciunt! Odio sed alias autem blanditiis iure ea soluta quasi
+            quibusdam voluptate numquam, incidunt sequi iste quaerat animi
+            expedita exercitationem laboriosam.
           </Typography>
         </div>
       </React.Fragment>
